ffcb08a86e37366eb93408a8b89436a65100395a
RaquelPintoR <raquelppr12@hotmail.com>
2021-05-10 18:48:30 -0600

    Cargado de etiquetas para filtrar y una parte de botón Filtrar en Grupos

--- src/app/pages/groups/groups.component.ts
+++ src/app/pages/groups/groups.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { GroupsService } from '../../services/groups.service';
+import { LabelsService } from '../../services/labels.service';
+
 import { ViewChild} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
 import {MatPaginator} from '@angular/material/paginator';
 import { SelectionModel} from '@angular/cdk/collections';
 import { FormControl } from '@angular/forms';
@@ -20,6 +24,7 @@ import { MatSort } from '@angular/material/sort';
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
+  providers: [GroupsService, LabelsService]
  
 })
 export class GroupsComponent implements OnInit {
@@ -31,21 +36,28 @@ export class GroupsComponent implements OnInit {
 
   tags = new FormControl();
 
-  tagsList: string[] = ['Python', 'Intro', 'Poo', 'Geometria', 'Recursividad', 'Fibonacci'];
+  //Aqui se guarda lo devuelto por el servicio
+  tagResult: Array<any> = [];
   
-  groups = new FormControl();
-
-  groupsList: string[] = ['group1', 'group2', 'group3', 'group3', 'group4', 'group5'];
-
   dataStudentsList = new MatTableDataSource([]);
   selection = new SelectionModel<any>(true, []);
-  displayedStudentsColumnsList: string[] = ['select','id', 'name', 'age', 'address', 'edit'];
+  //displayedStudentsColumnsList: string[] = ['select','id', 'name', 'age', 'address', 'edit'];
+  displayedStudentsColumnsList: string[] = ['select','name', 'edit'];
   isTableExpanded=false;
 
+  constructor(private groupsService: GroupsService, private labelsService: LabelsService, public dialog: MatDialog) {
+   this.displayedStudentsColumnsList = this.displayedStudentsColumnsList;
+   this.dataStudentsList = new MatTableDataSource(this.STUDENTS_DATA);
+   this.getTagNames();
+   // Aqui se carga lo devuelto por el servicio
+   //this.dataSource = new MatTableDataSource(this.tagResult);
+ }
+ /*
   constructor(){
    this.displayedStudentsColumnsList = this.displayedStudentsColumnsList;
    this.dataStudentsList = new MatTableDataSource(this.STUDENTS_DATA);
   }
+*/
 
   ngOnInit(): void {
    
@@ -72,9 +84,6 @@ export class GroupsComponent implements OnInit {
         this.dataStudentsList.data.forEach(row => this.selection.select(row));
 
   }
-
-
- 
   
   toggleTableRows(element) {
     this.isTableExpanded = !this.isTableExpanded;
@@ -88,6 +97,7 @@ export class GroupsComponent implements OnInit {
     });
     
   }
+
   checkboxLabel(row?: Element): string {
     if (!row) {
 
@@ -96,13 +106,182 @@ export class GroupsComponent implements OnInit {
 
     return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id }`;
   }
+
   getKeys(object): string[] {
    //console.log(object);
    return Object.keys(object);
  }
+
  onItemSelected(idx: number) {
    console.log(idx);
  }
+
+ getTagNames() {
+   this.labelsService.getTagNames().subscribe(
+     data => {
+       this.tagResult = data;
+       /*
+       for(var property in this.tagResult) {
+         console.log(property + "=" + this.tagResult[property]);
+      }*/
+     },
+     error => console.log("Error: ", error),
+   );
+ }
+
+ filterGroups(tags: Object) {
+   console.log(tags);
+ }
+
+ STUDENTS_DATA= [
+   {
+      "id":1,
+      "name":"Abby Jaskolski ",
+      "isExpanded":false,
+ 
+      "subjects":[
+         {
+            "name":"Bio",
+            "type":"Medical",
+            "grade":"A",
+            "Ocupacion": "Carpintero"
+            
+         },
+         {
+            "name":"Chemistry",
+            "type":"Medical",
+            "grade":"A",
+            "Ocupacion": "Carpintero"
+            
+         },
+         {
+            "name":"Physics",
+            "type":"Medical",
+            "grade":"A",
+            "Ocupacion": "Carpintero"
+            
+         }
+      ]
+   },
+   {
+      "id":2,
+      "name":"Jabari Fritsch",
+      "isExpanded":false,
+ 
+      "subjects":[
+         {
+            "name":"Bio2",
+            "type":"Medical2",
+            "grade":"A2",
+            "Ocupacion": "Carpintero2"
+           
+         },
+         {
+            "name":"Chemistry",
+            "type":"Medical",
+            "grade":"A",
+            "Ocupacion": "Carpintero"
+           
+         },
+         {
+            "name":"Physics",
+            "type":"Medical",
+            "grade":"A",
+            "Ocupacion": "Carpintero"
+            
+         }
+      ]
+   },
+   {
+      "id":3,
+      "name":"Maybell Simonis",
+      "isExpanded":false,
+   
+      "subjects":[
+         {
+            "name":"Bio",
+            "type":"Medical",
+            "grade":"A",
+            "Ocupacion": "Carpintero"
+            
+         },
+         {
+            "name":"Chemistry",
+            "type":"Medical",
+            "grade":"A",
+            "Ocupacion": "Carpintero"
+            
+         },
+         {
+            "name":"Physics",
+            "type":"Medical",
+            "grade":"A",
+            "Ocupacion": "Carpintero"
+           
+         }
+      ]
+   },
+   {
+      "id":4,
+     "name":"Abby Jaskolski ",
+     "isExpanded":false,
+     
+     "subjects":[
+        {
+           "name":"Bio",
+           "type":"Medical",
+           "grade":"A",
+           "Ocupacion": "Carpintero"
+           
+        },
+        {
+           "name":"Chemistry",
+           "type":"Medical",
+           "grade":"A",
+           "Ocupacion": "Carpintero"
+           
+        },
+        {
+           "name":"Physics",
+           "type":"Medical",
+           "grade":"A",
+           "Ocupacion": "Carpintero"
+       
+        }
+     ]
+  },   
+   {
+   "id":5,
+   "name":"Abby Jaskolski ",
+   "isExpanded":false,
+ 
+   "subjects":[
+      {
+         "name":"Bio",
+         "type":"Medical",
+         "grade":"A",
+         "Ocupacion": "Carpintero"
+ 
+      },
+      {
+         "name":"Chemistry",
+         "type":"Medical",
+         "grade":"A",
+         "Ocupacion": "Carpintero"
+ 
+      },
+      {
+         "name":"Physics",
+         "type":"Medical",
+         "grade":"A",
+         "Ocupacion": "Carpintero"
+ 
+      }
+   ]
+ },
+ ];
+
+ /*
  STUDENTS_DATA= [
    {
       "id":1,
@@ -260,6 +439,7 @@ export class GroupsComponent implements OnInit {
    ]
  },
  ];
+ */
 
 }
 
